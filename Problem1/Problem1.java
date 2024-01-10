6c167afd08c3ba109b15feca3462dc6a3d232dc8
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-01-10 12:48:37 +0530

    Add files via upload

---
+++ Problem1/Problem1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[]args){
+
+        // Average of three numbers
+
+        // Scanner sc = new Scanner(System.in);
+        // int A = sc.nextInt();
+        // int B = sc.nextInt();
+        // int C = sc.nextInt();
+
+        // int sum = A+B+C;
+        // int avg = sum/3;
+
+        // System.out.println("Average of three numbers:" + " " + avg);
+
+        // Area of square
+
+        // Scanner sc = new Scanner(System.in);
+
+        // int $ = sc.nextInt();
+        // int area = $ * $;
+
+        // System.out.println("Area of square" + " " + area);
+
+        // Total cost of bill
+
+        // Scanner sc = new Scanner(System.in);
+        
+        // float pencil = sc.nextFloat();
+        // float pen = sc.nextFloat();
+        // float eraser = sc.nextFloat();
+
+        // float total = pencil + pen + eraser;
+        // float gst = (total * 18)/100;
+
+        // float bill = total + gst;
+        // System.out.println("Total bill" + " " + bill);
+
+
+    }
+}
