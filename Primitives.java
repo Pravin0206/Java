4b9d5d0129ecb49d82fe4e660fb2ad38c529053a
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-01-10 11:12:48 +0530

    Add files via upload

---
+++ Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args) {
+        boolean isValid =true;
+        byte marks = 98;
+        double pi =3.14;
+        float div = 15.444f;
+        long views = 10000000l;
+        char gender = 'M';
+        System.out.println(gender);
+    }
+}
