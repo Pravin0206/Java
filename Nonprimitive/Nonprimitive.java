37e6011da7059350d63252695213c62a8ea93eca
Pravin Tangade <99708895+Pravin0206@users.noreply.github.com>
2024-01-10 11:21:29 +0530

    Add files via upload

---
+++ Nonprimitive/Nonprimitive.java
@@ -0,0 +1,15 @@
+class Point{
+    int x;
+    int y;
+}
+
+
+public class Nonprimitive {
+    public static void main(String[]args){
+        Point p = new Point();
+        p.x = 10;
+        p.y = 40;
+        System.out.println(p.x + " " + p.y);
+
+    }
+}
